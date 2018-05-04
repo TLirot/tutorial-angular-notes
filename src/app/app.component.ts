ee78aff73fbe419cfeb75a2b8184f535724ac00d
Toril92 <migue.toril@intelligenia.com>
2018-05-04 14:38:05 +0200

    Primera subida al respositorio del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'app'
+  my_notes = [];
+  note = { id: null, title: null, description: null };
+  showForm = false;
+  editing = false;
+  action: string = '';
+  addNote() {
+    this.showForm = true;
+    this.action = 'Nueva';
+    this.resetNote();
+  }
+  hideForm() {
+    this.showForm = false;
+  }
+  createNote() {
+    if (this.editing) {
+      //Editar
+      var me = this;
+      this.my_notes.forEach(function (el, i) {
+        if (el.id === me.note.id) {
+          me.my_notes[i] = me.note;
+        }
+      });
+    } else {
+      this.note.id = Date.now();
+      this.my_notes.push(this.note);
+    }
+    this.hideForm();
+  }
+  viewNote(note) {
+    this.editing = true;
+    this.action = 'Editar';
+    this.note = note;
+    this.showForm = true;
+  }
+
+  resetNote() {
+    this.note = { id: null, title: null, description: null };
+  }
 }
